1a2d5b110a56f69c769e74576fec04616924b263
Ahmed@Atef <clay674302@gmail.com>
2025-12-01 15:14:26 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Portfolio-Frontend/sw.js', { scope: '/Portfolio-Frontend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
